804f32929e4c851e825bc26d20b3b93ebfc7edb0
MAC <mac@KIMELMAC.local>
2021-11-18 16:20:58 +0900

    Add: 메인 컴포넌트 분리

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.scss';
 
-function Nav(props) {
+function Nav() {
   return (
     <header>
       <div className="NavContainer">
