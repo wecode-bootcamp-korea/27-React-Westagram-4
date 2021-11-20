f797453d7f60be4609fa74b4a632836d7fed83cc
MAC <mac@KIMELMAC.local>
2021-11-20 19:34:23 +0900

    Add: Mission 1~2 완료

--- src/pages/Heyri/Login/LoginHyeri.js
+++ src/pages/Heyri/Login/LoginHyeri.js
@@ -10,16 +10,15 @@ function LoginHyeri() {
     navigate('/main-hyeri');
   };
 
-  let [idValue, setIdInput] = useState({
-    id: '',
-    pw: '',
-  });
+  let [idValue, setIdInput] = useState('');
+  let [pwValue, setPwInput] = useState('');
   console.log(idValue);
+  console.log(pwValue);
 
-  const handleIdInput = e => {
-    const { name, value } = e.target;
-    setIdInput({ ...idValue, [name]: value });
-  };
+  // const handleInput = e => {
+  //   const { name, value } = e.target;
+  //   setIdInput({ ...idValue, [name]: value });
+  // };
 
   return (
     <>
@@ -33,7 +32,9 @@ function LoginHyeri() {
               id="id"
               name="id"
               placeholder="전화번호, 사용자 이름 또는 이메일"
-              onChange={handleIdInput}
+              onChange={e => {
+                setIdInput(e.target.value);
+              }}
             />
           </div>
           <div className="passForm">
@@ -42,12 +43,34 @@ function LoginHyeri() {
               id="pw"
               name="pw"
               placeholder="비밀번호"
-              onChange={handleIdInput}
+              onChange={e => {
+                setPwInput(e.target.value);
+              }}
             />
           </div>
-          <button type="button" id="loginBtn" onClick={goToMain} name="">
-            로그인
-          </button>
+
+          {idValue.includes('@') && pwValue.length >= 5 ? (
+            <button
+              type="button"
+              id="loginBtn"
+              onClick={goToMain}
+              name=""
+              style={{ backgroundColor: '#0095f6' }}
+            >
+              로그인
+            </button>
+          ) : (
+            <button
+              type="button"
+              id="loginBtn"
+              onClick={goToMain}
+              name=""
+              style={{ backgroundColor: '#b5d7fa' }}
+            >
+              로그인
+            </button>
+          )}
+
           <div className="bottomText">비밀번호를 잊으셨나요?</div>
         </form>
       </section>
